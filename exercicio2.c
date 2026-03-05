8c574c43ec70ebe63854ceaf3b39ab2b0c8220aa
Wladmir Luiz de Carvalho <wladmircarvalho@outlook.com>
2026-03-05 09:45:02 -0300

    Fatorial

--- exercicio2.c
+++ exercicio2.c
@@ -1,12 +1,28 @@
-int main() {
-    int n;
+#include <stdio.h>
 
-    printf("Digite um numero inteiro positivo N: ");
+int main() {
+   
+   int n;
+    printf("Digite um numero inteiro nao negativo N: ");
     scanf("%d", &n);
 
+    if (n < 0) {
+        printf("Por favor, digite um numero maior ou igual a zero.\n");
+        return 1;
+    }
+
+    int contador = 1;
+    for (int i = n; i >= 1; i--) {
+        contador = contador * i;   
+    }
+
+    printf("O fatorial de %d e: %d\n", n, contador);
 
 
-
-    printf("\n");
-
+    return 0;
 }
+
+
+
+
+
